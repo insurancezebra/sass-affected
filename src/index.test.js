5383cb2809cdd907ff90a02c41a3cfe3e3d8dfb9
John Apostol <notimetobeidle@gmail.com>
2018-04-26 13:52:37 -0500

    Update tests to use snapshots

--- src/index.test.js
+++ src/index.test.js
@@ -1,54 +1,28 @@
 import sassAffected from "./index";
 
-const wrap = roots => ({
-  missing: [],
-  roots
-});
-
-const rootA = {
-  file: "mocks/rootA.scss",
-  message: "There's a change in rootA.scss!"
-};
-
-const rootB = {
-  file: "mocks/rootB.scss",
-  message: "There's a change in rootB.scss!"
-};
-
-const rootC = {
-  file: "mocks/rootC.scss",
-  message: "There's a change in rootC.scss!"
-};
-
 it("works with a node that is the root", async () => {
-  expect(await sassAffected("mocks", ["mocks/rootA.scss"])).toEqual(
-    wrap([rootA])
-  );
+  expect(await sassAffected("mocks", ["mocks/rootA.scss"])).toMatchSnapshot();
 });
 
 it("works with a node imported by the root", async () => {
-  expect(await sassAffected("mocks", ["mocks/intermediaryA.scss"])).toEqual(
-    wrap([rootA])
-  );
+  expect(
+    await sassAffected("mocks", ["mocks/intermediaryA.scss"])
+  ).toMatchSnapshot();
 });
 
 it("works with a leaf node imported through an intermediary", async () => {
-  expect(await sassAffected("mocks", ["mocks/leafA.scss"])).toEqual(
-    wrap([rootA])
-  );
+  expect(await sassAffected("mocks", ["mocks/leafA.scss"])).toMatchSnapshot();
 });
 
 describe("with multiple roots", () => {
   it("works with a node imported by the root", async () => {
-    expect(await sassAffected("mocks", ["mocks/intermediaryB.scss"])).toEqual(
-      wrap([rootB, rootC])
-    );
+    expect(
+      await sassAffected("mocks", ["mocks/intermediaryB.scss"])
+    ).toMatchSnapshot();
   });
 
   it("works with a leaf node imported through an intermediary", async () => {
-    expect(await sassAffected("mocks", ["mocks/leafB.scss"])).toEqual(
-      wrap([rootB, rootC])
-    );
+    expect(await sassAffected("mocks", ["mocks/leafB.scss"])).toMatchSnapshot();
   });
 
   it("works with duplicate roots", async () => {
@@ -57,7 +31,7 @@ describe("with multiple roots", () => {
         "mocks/intermediaryB.scss",
         "mocks/leafB.scss"
       ])
-    ).toEqual(wrap([rootB, rootC]));
+    ).toMatchSnapshot();
   });
 });
 
@@ -65,7 +39,7 @@ describe("with mixed roots", () => {
   it("works with leaf nodes", async () => {
     expect(
       await sassAffected("mocks", ["mocks/leafA.scss", "mocks/leafB.scss"])
-    ).toEqual(wrap([rootA, rootB, rootC]));
+    ).toMatchSnapshot();
   });
 
   it("works with duplicate roots", async () => {
@@ -76,7 +50,7 @@ describe("with mixed roots", () => {
         "mocks/intermediaryA.scss",
         "mocks/intermediaryB.scss"
       ])
-    ).toEqual(wrap([rootA, rootB, rootC]));
+    ).toMatchSnapshot();
   });
 });
 
